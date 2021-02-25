0bd67bf62126739fa0f8671a4360392e2f04e52f
Andrea <andrea.behanova@it.uu.se>
2021-02-25 18:37:14 +0100

    creating functions

--- Project/registration/andrea_script.py
+++ Project/registration/andrea_script.py
@@ -1,13 +1,11 @@
 
 import register_example_with_adam as reg
-
 from skimage import io
 import numpy as np
 from os import listdir
 import scipy.ndimage
 from os.path import isfile, join
 import multiprocessing as mp
-import time
 from scipy import ndimage
 import filters
 
@@ -31,52 +29,21 @@ def sharpen(im,alpha,sigma1,sigma2):
     return np.clip(sharpened, a_min = 0.0, a_max = 1.0)
 
 
-
 def registr(all_paths):
     ref = all_paths[0]
     flo = all_paths[1]
     out = all_paths[2]
     reg.run(ref,flo,out)
 
-if __name__ == '__main__':
-    all_p = []
-    for i in range(len(flo_paths)):
-        p = [join(paths,ref_paths), join(paths,flo_paths[i]), out_paths+str(i+2).zfill(3)+'.tiff']
-        all_p.append(p)
-
-    #parallel registration
-    with mp.Pool(4) as p:
-        p.map(registr, all_p)
-
-    # merging registered images into 3D volume
-    regpath = 'c:/master/registration/dataset/registered'
-    regfiles = [f for f in listdir(regpath) if isfile(join(regpath, f))]
-
-    ref_file = join(paths,ref_paths)
-    ref_im = io.imread(ref_file, as_gray=True)
-    #ref_im = scipy.ndimage.zoom(ref_im, zoom=(2, 2), order=3)
-    final_vol = np.zeros((len(onlyfiles),ref_im.shape[0],ref_im.shape[1]))
-
-    #sharpening
-    sharpening = 0
-
-    if sharpening == 1:
-        sigma1 = 1
-        sigma2 = 0.5
-        alpha = 50
-        sharpened = sharpen(ref_im,alpha,sigma1,sigma2)
-    else:
-        sharpened = ref_im
-
+def vol_3D(regfiles, sharpening, alpha, sigma1, sigma2, ref_im, sharpened, regpath):
     final_vol[0,:,:] = sharpened
     x_start = 1
     x_end = len(ref_im[0])
     y_start = 1
     y_end = len(ref_im[1])
 
-    sum_mask = 0
     for i in range(len(regfiles)):
-        filee = './dataset/registered/' + regfiles[i]
+        filee = regpath + regfiles[i]
         reg_im = io.imread(filee, as_gray=True)
 
         #sharpening
@@ -109,10 +76,10 @@ if __name__ == '__main__':
 
     #crop mask in 3D
     crop_vol = final_vol[:,x_start+1:x_end-1,y_start+1:y_end-1]
+    return crop_vol
 
 
-    io.imsave('./dataset/3Dvol_pattern.tiff', (crop_vol).astype('uint16')) #*65535 if shapening
-
+def aggregation(crop_vol):
     # Aggregation
     agg_im_mean = np.zeros((crop_vol.shape[1],crop_vol.shape[2]))
     agg_im_median = np.zeros((crop_vol.shape[1],crop_vol.shape[2]))
@@ -121,6 +88,42 @@ if __name__ == '__main__':
         for j in range(crop_vol.shape[2]):
             agg_im_mean[i,j] = np.mean(crop_vol[:,i,j])
             agg_im_median[i,j] = np.median(crop_vol[:,i,j])
+    return agg_im_mean, agg_im_median
+
+if __name__ == '__main__':
+    all_p = []
+    for i in range(len(flo_paths)):
+        p = [join(paths,ref_paths), join(paths,flo_paths[i]), out_paths+str(i+2).zfill(3)+'.tiff']
+        all_p.append(p)
+
+    #parallel registration
+    with mp.Pool(4) as p:
+        p.map(registr, all_p)
+
+    # merging registered images into 3D volume
+    regpath = out_paths[:-3]
+    regfiles = [f for f in listdir(regpath) if isfile(join(regpath, f))]
+
+    ref_file = join(paths,ref_paths)
+    ref_im = io.imread(ref_file, as_gray=True)
+    final_vol = np.zeros((len(onlyfiles),ref_im.shape[0],ref_im.shape[1]))
+
+    #sharpening
+    sharpening = 0
+    sigma1 = 1
+    sigma2 = 0.5
+    alpha = 50
+
+    if sharpening == 1:
+        sharpened = sharpen(ref_im,alpha,sigma1,sigma2)
+    else:
+        sharpened = ref_im
+
+    crop_vol = vol_3D(regfiles, sharpening, alpha, sigma1, sigma2, ref_im, sharpened, regpath)
+
+    io.imsave('./dataset/3Dvol_pattern.tiff', (crop_vol).astype('uint16')) #*65535 if shapening
+
+    [agg_im_mean, agg_im_median] = aggregation(crop_vol)
 
     io.imsave('./dataset/agg_im_pattern_mean.tiff', (agg_im_mean).astype('uint16')) #*65535 if shapening
     io.imsave('./dataset/agg_im_pattern_median.tiff', (agg_im_median).astype('uint16')) #*65535 if shapening
